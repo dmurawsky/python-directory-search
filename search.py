da123d44afb1015ca9b6a4ca1467ecc5fde2960f
Daniel Murawsky <daniel.murawsky@stelligent.com>
2017-03-01 16:10:04 -0500

    improvements from Casey

--- search.py
+++ search.py
@@ -1,89 +1,87 @@
 import mmap
 import os
-from os import walk
 import re
 
-class SearchDir():
-    def __init__(self, args):
-        if 'directory' in args:
-            if args['directory'] != '.':
-                args['directory'] = os.getcwd()+'/'+args['directory']
-            else:
-                args['directory'] = os.getcwd()+'/'
-        self._args = args
-        if self.valid_args():
-            self._firstlen = len(args['first'])
-            self._secondlen = len(args['second'])
-            self._cwdlen = len(os.getcwd())
 
-    def get_files_in_dir(self):
+class SearchDir:
+    # Format the directory as an absolute path then search it
+    def get_resluts(self, args):
+        if self.__valid_args(args):
+            if args['directory'] != '.':
+                args['directory'] = os.getcwd() + '/' + args['directory']
+            else:
+                args['directory'] = os.getcwd() + '/'
+            return self.__search_all_files(args)
+        else:
+            print 'Invalid Arguements'
+
+    # Get all files in the directory the user selected
+    def __get_files_in_dir(self, dir):
         f = []
-        for (dirpath, dirnames, filenames) in walk(self._args['directory']):
-            f.append({'dir':dirpath, 'files':filenames})
+        for (dirpath, dirnames, filenames) in os.walk(dir):
+            f.append({'dir': dirpath, 'files': filenames})
         return f
 
-    def get_search_text(self, path, indexes):
-        f = open(path)
-        s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-        first = min(indexes)
-        second = max(indexes) + max(self._firstlen, self._secondlen)
-        return '... '+s[first:second].replace('\n', ' ')+' ...'
-
-    def print_results(self):
-        files = self.search_all_files()
-        for f in files:
-            if len(f['results']) > 0:
-                print '\n\n'+f['file']+' :'
-                for r in f['results']:
-                    print '\t'+r
-        print '\n'
-
-    def search_all_files(self):
-        files = []
-        for fldr in self.get_files_in_dir():
+    # Recursively search all files and folders for .txt files then search each file
+    def __search_all_files(self, args):
+        res = []
+        for fldr in self.__get_files_in_dir(args['directory']):
             for f in fldr['files']:
                 if f[-4:] == '.txt':
-                    results = []
-                    path = fldr['dir']+'/'+f
-                    d = self.distances_between_strings(path)
-                    for i in d:
-                        results.append(self.get_search_text(path,i))
-                    files.append({'file':path[self._cwdlen:], 'results':results})
-        return files
+                    path = fldr['dir'] + '/' + f
+                    res.append(SearchFile().get_file_results(path, args))
+        return res
 
-    def search_file_for_string(self, path, str):
-        f = open(path)
-        s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-        return [m.start() for m in re.finditer(self._args[str], s)]
-
-    def distances_between_strings(self, file):
-        first = self.search_file_for_string(file, 'first')
-        second = self.search_file_for_string(file, 'second')
-        return self.distances(first, second)
-
-    def distances(self, first, second):
-        dists = []
-        for f in first:
-            for s in second:
-                d = abs(f-s)
-                if d<=int(self._args['context']):
-                    dists.append([f,s])
-        return dists
-
-    def valid_args(self):
-        if 'directory' not in self._args:
+    # Check if required arguements exist in args dictionary
+    def __valid_args(self, args):
+        if 'directory' not in args:
             return False
-        if 'context' not in self._args:
+        if 'context' not in args:
             return False
-        if 'first' not in self._args:
+        if 'first' not in args:
             return False
-        if 'second' not in self._args:
+        if 'second' not in args:
             return False
         return True
 
-    def print_args(self):
-        print self._args
-        return
+
+class SearchFile:
+    # Opens the file, then, prints the file name and results of the search
+    def get_file_results(self, path, args):
+        with open(path) as f:
+            s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+            d = self.__distances(s, args['first'], args['second'], args['context'])
+            res = {'name': '\n\n' + path[len(os.getcwd()):] + ' :', 'results': []}
+            for i in d:
+                first = min(i)
+                second = max(i) + max(len(args['first']), len(args['second']))
+                res['results'].append('\t... ' + s[first:second].replace('\n', ' ') + ' ...')
+        f.closed
+        return res
+
+    # Gets all occurances of both strings in the file, then, gets the distances between each occurance
+    def __distances(self, text, string1, string2, context):
+        first = [m.start() for m in re.finditer(string1, text)]
+        second = [m.start() for m in re.finditer(string2, text)]
+        dists = []
+        for f in first:
+            for s in second:
+                d = abs(f - s)
+                if d <= int(context):
+                    dists.append([f, s])
+        return dists
+
+
+class PrintResults:
+    def __init__(self, results):
+        for r in results:
+            print r['name']
+            for f in r['results']:
+                print f
+        print '\n'
+
+
+# Get arguements from command line if script is run directly
 if __name__ == "__main__":
     import sys
 
@@ -92,19 +90,18 @@ if __name__ == "__main__":
         first = raw_input('First search phrase: ')
         second = raw_input('Second search phrase: ')
         context = raw_input('Max distance between phrases: ')
-        return {'directory':directory, 'context':context, 'first':first, 'second':second}
+        return {'directory': directory, 'context': context, 'first': first, 'second': second}
 
     def get_args():
-        return {'directory':sys.argv[1], 'first':sys.argv[2], 'second':sys.argv[3], 'context':sys.argv[4]}
+        return {'directory': sys.argv[1], 'first': sys.argv[2], 'second': sys.argv[3], 'context': sys.argv[4]}
 
     def init(args):
-        search = SearchDir(args)
-        # search.print_args()
-        search.print_results()
+        PrintResults(SearchDir().get_resluts(args))
 
-    if len(sys.argv)==1:
+    # If no arguements are passed inline, prompt user for input otherwise return args in custom dictionary
+    if len(sys.argv) == 1:
         init(get_input())
-    elif len(sys.argv)==5:
+    elif len(sys.argv) == 5:
         init(get_args())
     else:
         print 'Invalid syntax:\nInline Usage: python search.py <directory> <first_phrase> <second_phrase> <context>\nInput Usage: python search.py'
--- tests.py
+++ tests.py
@@ -1,35 +1,65 @@
 import unittest
 import os
-from searchDir import SearchDir
+import pep8
+from search import SearchDir
+from search import SearchFile
 
 class SearchDirTests(unittest.TestCase):
 
-    def test_search_file_for_string(self):
-        search = SearchDir({'directory':'test-dir', 'context':100, 'first':'Apollo who shoots afar', 'second':'the gods tremble before him'})
-        result = search.search_file_for_string(os.getcwd()+'/test-dir/appolo.txt', 'first')
-        self.assertEqual(result[0], 44)
-        result = search.search_file_for_string(os.getcwd()+'/test-dir/appolo.txt', 'second')
-        self.assertEqual(result[0], 106)
+    def test_pep8_conformance(self):
+        sg = pep8.StyleGuide(quiet=True)
+        sg.options.ignore += ('E501',)
+        sg.options.max_line_length = 100
+        res = sg.check_files(['search.py'])
+        self.assertEqual(res.total_errors, 0, "PEP8 Style Violations: Run 'pep8 search.py' in your terminal for details.")
 
-    def test_valid_args(self):
-        search = SearchDir({'directory':'test-dir', 'context':100, 'first':'Apollo who shoots afar', 'second':'the gods tremble before him'})
-        result = search.valid_args()
-        self.assertTrue(result)
+    def test_get_resluts(self):
+        res = SearchDir().get_resluts({'directory':'test-dir', 'context':100, 'first':'Apollo who shoots afar', 'second':'the gods tremble before him'})
+        self.assertEqual(len(res[0]['results']), 1,
+            "Expected to find one occurance of 'Apollo who shoots afar' within 100 characters of 'the gods tremble before him'")
 
-    def test_invalid_args(self):
-        first = SearchDir({'directory':'test', 'context':100, 'second':'test'})
-        self.assertFalse(first.valid_args())
-        second = SearchDir({'directory':'test', 'context':100, 'first':'test'})
-        self.assertFalse(second.valid_args())
-        context = SearchDir({'directory':'test', 'first':'test', 'second':'test'})
-        self.assertFalse(context.valid_args())
-        directory = SearchDir({'context':100, 'first':'test', 'second':'test'})
-        self.assertFalse(directory.valid_args())
+    def test_get_resluts(self):
+        res = SearchFile().get_file_results(os.getcwd() + '/' + 'test-dir/appolo.txt',
+            {'directory':'test-dir', 'context':100, 'first':'Apollo who shoots afar', 'second':'the gods tremble before him'})
+        self.assertEqual(len(res['results']), 1,
+            "Expected to find one occurance of 'Apollo who shoots afar' within 100 characters of 'the gods tremble before him'")
 
-    def test_distances_between_strings(self):
-        search = SearchDir({'directory':'test-dir', 'context':100, 'first':'Apollo who shoots afar', 'second':'the gods tremble before him'})
-        result = search.distances_between_strings(os.getcwd()+'/test-dir/appolo.txt')
-        self.assertTrue(result)
+    def test_search_all_files(self):
+        res = SearchDir().search_all_files(os.getcwd() + '/' + 'test-dir')
+
+    # def test_search_all_files(self):
+    # def test_valid_args(self):
+
+    # def test_get_file_results(self):
+    # def test_distances(self):
+
+
+    # def test_search_file_for_string(self):
+    #     search = SearchDir({'directory':'test-dir', 'context':100, 'first':'Apollo who shoots afar', 'second':'the gods tremble before him'})
+    #     result = search.search_file_for_string(os.getcwd()+'/test-dir/appolo.txt', 'first')
+    #     self.assertEqual(result[0], 44)
+    #     result = search.search_file_for_string(os.getcwd()+'/test-dir/appolo.txt', 'second')
+    #     self.assertEqual(result[0], 106)
+    #
+    # def test_valid_args(self):
+    #     search = SearchDir({'directory':'test-dir', 'context':100, 'first':'Apollo who shoots afar', 'second':'the gods tremble before him'})
+    #     result = search.valid_args()
+    #     self.assertTrue(result)
+    #
+    # def test_invalid_args(self):
+    #     first = SearchDir({'directory':'test', 'context':100, 'second':'test'})
+    #     self.assertFalse(first.valid_args())
+    #     second = SearchDir({'directory':'test', 'context':100, 'first':'test'})
+    #     self.assertFalse(second.valid_args())
+    #     context = SearchDir({'directory':'test', 'first':'test', 'second':'test'})
+    #     self.assertFalse(context.valid_args())
+    #     directory = SearchDir({'context':100, 'first':'test', 'second':'test'})
+    #     self.assertFalse(directory.valid_args())
+    #
+    # def test_distances_between_strings(self):
+    #     search = SearchDir({'directory':'test-dir', 'context':100, 'first':'Apollo who shoots afar', 'second':'the gods tremble before him'})
+    #     result = search.distances_between_strings(os.getcwd()+'/test-dir/appolo.txt')
+    #     self.assertTrue(result)
 
 if __name__ == '__main__':
     unittest.main()
